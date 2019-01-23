53a3283990bc4c7ab7954987959a3c3f1a232a93
Drew Heavner <veedubusc@gmail.com>
2019-01-23 17:18:37 -0500

    Initial Commit

---
+++ library/src/main/java/com/ftinc/harbinger/Harbinger.kt
@@ -0,0 +1,13 @@
+package com.ftinc.harbinger
+
+/**
+ * > a person or thing that announces or signals the approach of another.
+ *
+ * Schedule alarms and notifications through this single source to keep track and ensure that no id's get
+ * contaminated or duplicated
+ */
+object Harbinger {
+
+
+
+}
