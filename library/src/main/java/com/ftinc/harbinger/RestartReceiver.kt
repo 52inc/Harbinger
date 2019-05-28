76f49cc45ad4b45a1471e1c8eeb8dd536925eb6e
Drew Heavner <veedubusc@gmail.com>
2019-05-28 10:14:53 -0400

    Updated reboot receiver to cover more use cases

--- library/src/main/java/com/ftinc/harbinger/RestartReceiver.kt
+++ library/src/main/java/com/ftinc/harbinger/RestartReceiver.kt
@@ -9,7 +9,11 @@ class RestartReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent?) {
         intent?.let {
-            if (it.action == Intent.ACTION_MY_PACKAGE_REPLACED || it.action == Intent.ACTION_BOOT_COMPLETED) {
+            if (it.action == Intent.ACTION_MY_PACKAGE_REPLACED
+                || it.action == Intent.ACTION_REBOOT
+                || it.action == "android.intent.action.QUICKBOOT_POWERON"
+                || it.action == "com.htc.intent.action.QUICKBOOT_POWERON"
+                || it.action == Intent.ACTION_BOOT_COMPLETED) {
                 Harbinger.rescheduleJobs()
             }
         }
